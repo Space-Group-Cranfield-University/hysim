0dddacdb3c1ab8427c980fefdc9fcf4c5ccecaf6
Cameron <cameron.f.leslie@cranfield.ac.uk>
2022-10-24 17:35:28 +0100

    Added sensor builder class

---
+++ hyperspacesim/sim/builders.py
@@ -0,0 +1,37 @@
+'''Classes to construct complex chaser and target instances'''
+
+class SensorBuilder:
+    '''Build sensor from camera, film and spectrum input files'''
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset():
+        self.
+
+
+
+
+class SceneBuilder:
+    '''Constructs the overall scene object'''
+    def __init__(self):
+        pass
+
+    def set_integrator(self):
+        pass
+
+    def choose_sensor(self):
+        pass
+
+    def import_model(self):
+        pass
+
+    def import_shape(self):
+        pass
+
+    def set_chaser_location(self):
+        pass
+
+    def set_target_location(self):
+        pass
+
+
