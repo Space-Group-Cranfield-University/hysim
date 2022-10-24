00133153cf86bf4e8ebaf40fb26ddd5990b7b345
Cameron Leslie <cameron.f.leslie@cranfield.ac.uk>
2022-10-24 22:22:48 +0100

    Formattin

--- hyperspacesim/test.py
+++ hyperspacesim/test.py
@@ -10,34 +10,30 @@ if __name__ == "__main__":
 
     print("\nTesting...\n\n")
 
-spectrum_path = data_paths.get_spectrum_path("sensors", "VNIR")
-print("Spectrum Path:\n")
-print(spectrum_path)
+sensor_selection = "VNIR"
+film_resolution = (1920,1080)
+fov = 3.8
+aperture = 0.2
+focus_distance = 100.0
 
+#### Commands ####
+spectrum_path = data_paths.get_spectrum_path("sensors", "VNIR")
 
 spectrum_data = spd_reader.SPDReader(spectrum_path)
-print("\n\nSpectrum Object:\n")
-#print(spectrum_data.wavelengths)
-#pprint(spectrum_data.values)
 
 hyperspectral_bands = spectrum.FilmSensitivitySpectrum(
     spectrum_data.wavelengths,
     spectrum_data.values
 )
-print("\n\Spectrum Dict:\n")
-#pprint(hyperspectral_film.build_dict())
 
+# Create film
 film = sensors.SpectralFilm(hyperspectral_bands)
-print("\n\Film Dict:\n")
-#pprint(film)
 
+# Build camera
 camera = sensors.ThinLenseCamera(
     field_of_view=3.8,
     aperture_radius=0.2,
     focus_distance=100.0
     )
-#print(camera)
 
 vnir_sensor = sensors.SpectralSensor(film, camera)
-#pprint(vnir_sensor)
-pprint(len(hyperspectral_bands))
