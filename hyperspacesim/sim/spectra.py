2343c73d7b9f6725b197f6398a4d420c407bb01a
Cameron Leslie <cameron.f.leslie@cranfield.ac.uk>
2022-11-06 11:23:15 +0000

    Added irradience spectrum

--- hyperspacesim/sim/spectra.py
+++ hyperspacesim/sim/spectra.py
@@ -9,17 +9,44 @@ from dataclasses import dataclass
 @dataclass
 class Spectrum(ABC):
     '''A spectrum wavelengths'''
-    wavelengths: list[float]
+    wavelengths: list
 
     def __len__(self):
         '''returns the length of the spectrum'''
         return len(self.wavelengths)
 
+    def crop_spectrum(self, min_wavelength, max_wavelength):
+        '''Crops spectrum to an upper and lower value'''
+        pass
+
+    def resize_spectrum(self, spectrum_size):
+        '''Creates polynomial of spectrum and remakes it with a given size'''
+        pass
+
+    def create_value_comma_string(self, values: list):
+        '''Returns a string of comma seperated values'''
+        string_values = [str(value) + "," for value in values]
+        return "".join(string_values)[:-1]
+
+
+@dataclass
+class IrradianceSpectrum(Spectrum):
+    '''A spectrum of irradiance for given wavelengths'''
+    irradiance: list
+
+    def build_dict(self):
+        return {
+            "type": "spectrum",
+            "wavelengths": self.create_value_comma_string(self.wavelengths),
+            "values": self.create_value_comma_string(self.irradiance),
+        }
+
+
 
 @dataclass
 class FilmSensitivitySpectrum(Spectrum):
-    '''A spectrum of film sensitivities and corresponding wavelengths'''
-    sensitivities: list[float]
+    '''A spectrum of film sensitivities for given wavelengths'''
+    sensitivities: list
 
     def band_name(self, lower_value, upper_value):
         '''Creates string band name from two values with an _ between'''
