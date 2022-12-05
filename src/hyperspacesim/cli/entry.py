f31dcbda7dd9178da99ac336a10cf4691176bff6
Cameron <cameron.f.leslie@cranfield.ac.uk>
2022-12-05 22:09:54 +0000

    Changed run path to unix

--- src/hyperspacesim/cli/entry.py
+++ src/hyperspacesim/cli/entry.py
@@ -5,4 +5,9 @@ from hyperspacesim import sim
 def main():
     """Entry point main function"""
     run_directory = Path.cwd()
+    run_directory = str(run_directory).replace("\\", "/")
     sim.run_sim(run_directory)
+
+
+if __name__ == "__main__":
+    main()
