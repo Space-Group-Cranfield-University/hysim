a6e1801f23ed5b11485aec05ae8963ff74e6093a
Odotocodot <48138990+Odotocodot@users.noreply.github.com>
2025-04-11 18:31:44 +0100

    Add support for multiple renders

--- src/hysim/sim.py
+++ src/hysim/sim.py
@@ -3,40 +3,109 @@
 This module contains the main run function for the simulator and classes to
 handle Mitsuba.
 """
+
 # Debugging
 # import pretty_errors
 
 # Logging
 import logging
-from pathlib import Path
 
 # Packages
 import mitsuba as mi
+import mitsuba.scalar_rgb as mit
+import spiceypy as spice
 
 # I/O
-from hysim import frame_transforms as frames, scene_builder as sb
+from pathlib import Path
 
 # Package data
 from hysim.data import data_handling as dh
+from hysim import scene_builder as sb
+from hysim import frame_transforms as ft
 
 # Simulator
 from hysim import output_data
 from hysim.configs.config import Config
 
 
-def _set_mitsuba_logger():
-    """Creates a custom mitsuba formatter to match HySim and sets mitsuba's logger to use
-    it. Also sets the log level to mi.LogLevel.Info to make sure the start and finished
-    rendering messages are displayed."""
-    class CustomMitsubaFormatter(mi.Formatter):
-        def format(self, level: mi.LogLevel, thread, class_, file, line, msg):
-            return f" {level.name.upper():8} Mitsuba - {msg}"
+class RenderInstance:
+    """Represents an instantaneous snapshot of the scene at a specified epoch"""
 
-    # Note:  there is no progress bar if mi.variant() is noy a scalar varint
-    mitsuba_logger = mi.Thread.thread().logger()
-    mitsuba_logger.set_formatter(CustomMitsubaFormatter())
-    mitsuba_logger.set_log_level(mi.LogLevel.Info)
-    del mitsuba_logger
+    output: mit.TensorXf
+    scene_builder: sb.SceneBuilder
+    position_data: ft.PositionData
+
+    def __init__(self, config: Config, epoch: float):
+        self.epoch = epoch
+        self._config = config
+
+    def build_scene(self):
+        self.position_data = ft.PositionData(self._config.mission, self.epoch)
+        self.scene_builder = sb.SceneBuilder(self._config, self.position_data)
+
+    def render(self) -> mit.TensorXf:
+        scene_dict = self.scene_builder.scene.asdict()
+        sim: mit.Scene = mi.load_dict(scene_dict)
+        print(mi.traverse(sim))
+        self.output = mi.render(sim)
+        return self.output
+
+
+class RenderController:
+    output: mit.TensorXf
+    def __init__(self, config: Config, runs: int):
+        self._config = config
+        self.runs = runs
+        self.base_epoch = spice.str2et(config.mission.datetime)
+        self.instances = [RenderInstance(config, self.base_epoch)]
+
+    def build_scenes(self):
+        logging.info("Calculating scene geometry from orbit data")
+        logging.info("Building scenes")
+        for instance in self.instances:
+            instance.build_scene()
+            # logging.debug(f"Chaser ECI Coordinates: {instance.position_data.chaser_position}")
+            # logging.info("Relative distance to target: %0.2fm", instance.position_data.relative_distance)
+            # logging.debug("Final Scene Dictionary...")
+            # logging.debug(instance.scene_builder.scene.asdict())
+
+    def render(self) -> mit.TensorXf:
+        logging.info("Adding case directory search paths to mitsuba")
+        file_resolver = mi.Thread.thread().file_resolver()
+
+        for path in self._config.directories:
+            file_resolver.append(path)
+            logging.debug("Added: " + path)
+        del file_resolver
+
+        self._set_mitsuba_logger()
+        # logging.info("Loading scenes into Mitsuba")
+        # logging.info("Running Mitsuba")
+        # print("")
+        self.output = self.instances[0].render()
+
+        for instance in self.instances[1:]:
+            self.output += instance.render()
+        # print("")
+        logging.info("Render complete")
+        return self.output
+
+    @staticmethod
+    def _set_mitsuba_logger():
+        """Creates a custom mitsuba formatter to match HySim and sets mitsuba's logger to use
+        it. Also sets the log level to mi.LogLevel.Info to make sure the start and finished
+        rendering messages are displayed."""
+
+        class CustomMitsubaFormatter(mi.Formatter):
+            def format(self, level: mi.LogLevel, thread, class_, file, line, msg):
+                return f" {level.name.upper():8} Mitsuba - {msg}"
+
+        # Note:  there is no progress bar if mi.variant() is noy a scalar varint
+        mitsuba_logger = mi.Thread.thread().logger()
+        mitsuba_logger.set_formatter(CustomMitsubaFormatter())
+
+        mitsuba_logger.set_log_level(mi.LogLevel.Info)
+        del mitsuba_logger
 
 
 def run_sim(run_directory: Path):
@@ -58,7 +127,7 @@ def run_sim(run_directory: Path):
         files and user data.
 
     """
-    logging.info(f"Running Simulation Case @ \"{run_directory}\"");
+    logging.info(f'Running Simulation Case @ "{run_directory}"')
 
     # ------------------------------- #
     # Get user Inputs
@@ -66,52 +135,23 @@ def run_sim(run_directory: Path):
     logging.info("Getting user inputs from configuration files")
 
     config = Config(run_directory)
-    kernel_paths = dh.get_kernel_paths()
+
+    logging.info("Setting up SPICE kernels")
+    spice.furnsh(dh.get_kernel_paths())
 
     mi.set_variant(config.case.mitsuba_variant)
 
-
-    logging.info("Calculating scene geometry from orbit data")
-    position_data = frames.ScenePositionData(config.mission, kernel_paths)
-
-    # ------------------------------- #
-    # Assemble Scene
-    # ------------------------------- #
-    logging.info("Building scene")
-    scene_builder = sb.SceneBuilder(config, position_data)
-
-    logging.debug(f"Chaser ECI Coordinates: {position_data.chaser_position}")
-    logging.info("Relative distance to target: %0.2fm", position_data.relative_distance)
-
-    # ------------------------------- #
-    # Load to mitsuba and run
-    # ------------------------------- #
-    scene_dict = scene_builder.scene.asdict()
-    logging.debug("Final Scene Dictionary...")
-    logging.debug(scene_dict)
-
-    logging.info("Adding case directory search paths to mitsuba")
-    file_resolver = mi.Thread.thread().file_resolver()
-    for path in config.directories:
-        file_resolver.append(path)
-        logging.debug("Added: " + path)
-    del file_resolver
-
-    logging.info("Loading scene into Mitsuba")
-    sim = mi.load_dict(scene_dict)
-    logging.info("Scene assembled successfully")
-    logging.info("Running Mitsuba")
-
-    _set_mitsuba_logger()
-    print("")
-    render: mi.TensorXf = mi.render(sim)
-    print("")
+    render_control = RenderController(config, 1)
+    render_control.build_scenes()
+    render_control.render()
 
     logging.info("Render complete")
     # ------------------------------- #
     # Export Outputs
     # ------------------------------- #
-    output = output_data.OutputHandler(render, scene_builder, config)
+    output = output_data.OutputHandler(
+        render_control.output, render_control.instances[0].scene_builder, config
+    )
     output.export_data()
 
     logging.info("Done")
