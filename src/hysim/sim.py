5231a400c197e5aa0ee9f56ca86b1c5d7decd11b
Odotocodot <48138990+Odotocodot@users.noreply.github.com>
2025-04-13 07:53:18 +0100

    Update logging for multiple renders

--- src/hysim/sim.py
+++ src/hysim/sim.py
@@ -9,6 +9,8 @@ handle Mitsuba.
 
 # Logging
 import logging
+from time import monotonic_ns as get_time
+from contextlib import contextmanager
 
 # Packages
 import mitsuba as mi
@@ -28,6 +30,33 @@ from hysim import frame_transforms as ft
 from hysim.configs.config import Config
 
 
+class CustomMitsubaFormatter(mi.Formatter):
+    def __init__(self, frame_index: int):
+        super().__init__()
+        self.frame_index = frame_index
+
+    def format(self, level: mi.LogLevel, thread, class_, file, line, msg):
+        return f" {level.name.upper():8} {chr(0x02523)}{chr(0x02501)} Frame {self.frame_index} - {msg}"
+
+    @staticmethod
+    @contextmanager
+    def use(frame_index: int):
+        """Creates a custom mitsuba formatter to match HySim and sets mitsuba's logger to use
+        it. Also sets the log level to mi.LogLevel.Info to make sure the start and finished
+        rendering messages are displayed."""
+
+        # NOTE: There is no progress bar displayed in the console if mi.variant() is not a scalar variant
+        mitsuba_logger = mi.Thread.thread().logger()
+        log_level = mitsuba_logger.log_level()
+        try:
+            mitsuba_logger.set_formatter(CustomMitsubaFormatter(frame_index))
+            mitsuba_logger.set_log_level(mi.LogLevel.Info)
+            yield
+        finally:
+            mitsuba_logger.set_log_level(log_level)
+            del mitsuba_logger
+
+
 class RenderInstance:
     """Represents an instantaneous snapshot (a frame) of the scene at a specified epoch"""
 
@@ -43,19 +72,15 @@ class RenderInstance:
         self.position_data = ft.PositionData(self._config.mission, self.epoch)
         self.scene_builder = sb.SceneBuilder(self._config, self.position_data)
 
-    def render(self) -> mit.TensorXf:
+    def render(self, frame_index: int = 0) -> mit.TensorXf:
         scene_dict = self.scene_builder.scene.asdict()
         sim: mit.Scene = mi.load_dict(scene_dict)
-        print(mi.traverse(sim))
-        self.output = mi.render(sim)
+        with CustomMitsubaFormatter.use(frame_index):
+            self.output = mi.render(sim)
         return self.output
 
 
 class RenderController:
-    class CustomMitsubaFormatter(mi.Formatter):
-        def format(self, level: mi.LogLevel, thread, class_, file, line, msg):
-            return f" {level.name.upper():8} Mitsuba - {msg}"
-
     output: mit.TensorXf
 
     def __init__(self, config: Config):
@@ -64,8 +89,8 @@ class RenderController:
         self.base_epoch = spice.str2et(config.mission.datetime)
         self.dt = config.sensor.camera.shutter_time / self.frame_count
         self.frames = [
-            RenderInstance(config, ft.Epoch(self.base_epoch, self.dt * frame_index)) for frame_index in
-            range(self.frame_count)
+            RenderInstance(config, ft.Epoch(self.base_epoch, self.dt * frame_index))
+            for frame_index in range(self.frame_count)
         ]
 
     def build_scenes(self):
@@ -84,33 +109,25 @@ class RenderController:
 
         for path in self._config.directories:
             file_resolver.append(path)
-            logging.debug("Added: " + path)
+            logging.debug(f"{chr(0x02523)}{chr(0x02501)} {path}")
+        else:
+            logging.debug(f"{chr(0x02517)}{chr(0x02501)} {path}")
         del file_resolver
 
-        self._set_mitsuba_logger()
-        # logging.info("Loading scenes into Mitsuba")
-        # logging.info("Running Mitsuba")
-        # print("")
+        # logging.info("Loading scene(s) into Mitsuba")
+        logging.info("Running Mitsuba")
         self.output = self.frames[0].render() * self.dt
 
-        for instance in self.frames[1:]:
-            self.output += instance.render() * self.dt
-        # print("")
-        logging.info("Render complete")
+        t0 = get_time()
+        for i, instance in enumerate(self.frames[1:]):
+            # logging.info(chr(0x02501))
+            self.output += instance.render(i + 1) * self.dt
+        t = (get_time() - t0) / 1e9
+        duration = ""
+        if round(t, 1) > 0 and self.frame_count > 1:
+            duration = f" (took {t:.2f}s)"
+        logging.info(f"Renders complete.{duration}")
         return self.output
-
-    @staticmethod
-    def _set_mitsuba_logger():
-        """Creates a custom mitsuba formatter to match HySim and sets mitsuba's logger to use
-        it. Also sets the log level to mi.LogLevel.Info to make sure the start and finished
-        rendering messages are displayed."""
-
-        # Note:  there is no progress bar if mi.variant() is noy a scalar varint
-        mitsuba_logger = mi.Thread.thread().logger()
-        mitsuba_logger.set_formatter(RenderController.CustomMitsubaFormatter())
-
-        mitsuba_logger.set_log_level(mi.LogLevel.Info)
-        del mitsuba_logger
 
 
 def run_sim(run_directory: Path):
@@ -149,8 +166,6 @@ def run_sim(run_directory: Path):
     render_control = RenderController(config)
     render_control.build_scenes()
     render_control.render()
-
-    logging.info("Render complete")
     # ------------------------------- #
     # Export Outputs
     # ------------------------------- #
